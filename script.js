969b000a0702da6c7c17727a5b2653b3174eaac9
areebalii <areebktk8@gmail.com>
2025-07-16 23:45:34 +0500

    add active link functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const downloadCv = document.querySelector('.download');
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
 
 // event listener download button
 downloadCv.addEventListener('click', () => {
@@ -6,4 +8,25 @@ downloadCv.addEventListener('click', () => {
   link.href = 'Areeb_Ali_CV.pdf';
   link.download = 'Areeb Ali Resume.pdf';
   link.click();
+});
+
+// add active class functionality to navigation links
+window.addEventListener('scroll', function () {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= (sectionTop - 300)) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
 });
